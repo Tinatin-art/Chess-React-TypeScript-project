9c04a4d1dce8d59d40702acfa1ac053643b18c8b
Tinatin <omurovatinatin@gmail.com>
2022-07-06 17:35:40 +0600

    chess project start

---
+++ src/models/figures/King.ts
@@ -0,0 +1,45 @@
+import { Cell } from "../Cell";
+import { Colors } from "../Colors";
+import { Figure, FigureNames } from "./Figure";
+import blackLogo from '../../assets/black-king.png';
+import whiteLogo from '../../assets/white-king.png';
+
+export class King extends Figure {
+
+    isFirstStep: boolean = true;
+    
+    constructor(color: Colors, cell: Cell) {
+        super(color, cell);
+        this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
+        this.name = FigureNames.KING
+    }
+
+    canMove(target: Cell): boolean {
+        if (!super.canMove(target))
+            return false
+
+        const direction = this.cell.figure?.color === Colors.BLACK ? 1 : -1;
+   
+
+        if ((target.y === this.cell.y + direction) && target.x === this.cell.x && this.cell.board.getCell(target.x, target.y).isEmpty()){
+            return true;
+        }
+        if(target.y === this.cell.y + direction && (target.x === this.cell.x + 1 || target.x === this.cell.x - 1) && this.cell.isEnemy(target)){
+                return true;
+        }
+
+        if ((target.x === this.cell.x + direction) && target.y === this.cell.y && this.cell.board.getCell(target.y, target.x).isEmpty()){
+            return true;
+        }
+        if(target.x === this.cell.x + direction && (target.y === this.cell.y + 1 || target.y === this.cell.y - 1) && this.cell.isEnemy(target)){
+                return true;
+        }
+        return false;
+    }
+
+    
+    moveFigure(target: Cell): void {
+        super.moveFigure(target);
+        this.isFirstStep = false;
+    }
+}
